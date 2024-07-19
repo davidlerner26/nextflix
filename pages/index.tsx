01cf1ade3ad987dd95bae5d39fcf41c301fabde1
davidlerner26 <david.lerner@hotmail.com>
2024-07-19 19:30:46 -0300

    Build navBar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 import Banner from "../components/banner/banner";
+import NavBar from "@/components/navbar/navbar";
 
 export default function Home() {
   return (
@@ -10,16 +11,13 @@ export default function Home() {
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Netflix</h1>
 
+      <NavBar username="ankita@ank.com" />
       <Banner
         title="Clifford the red dog"
         subTitle="a very cute dog"
         imgUrl="/static/clifford.webp"
       />
-
-      {/* <Navbar />
-      <Card /> */}
     </div>
   );
 }
