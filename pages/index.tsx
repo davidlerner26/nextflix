54db0c3cdebfc6357d7185151e41426e82b4106a
davidlerner26 <david.lerner@hotmail.com>
2024-07-26 11:40:46 -0300

    Configure Hasura to facilitate GraphQL requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Card from "../components/card/card";
 import SectionCards from "../components/card/section-cards";
 
 import { getPopularVideos, getVideos } from "../lib/videos";
+import { startFetchMyQuery } from "../lib/db/hasura";
 
 export async function getServerSideProps(context) {
   const disneyVideos = await getVideos("disney trailer");
@@ -27,6 +28,7 @@ export default function Home({
   productivityVideos,
   popularVideos,
 }) {
+  startFetchMyQuery();
   return (
     <div className={styles.container}>
       <Head>
